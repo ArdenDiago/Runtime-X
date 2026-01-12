4d7df07fb698ff3957d6ea98b2d8d09f89536546
GIT_NAME <diagoarden@gmail.com>
2026-01-12 22:58:45 +0530

    updated the main folder file

---
+++ cmd/worker/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"time"
+
+	"runtimex/worker"
+)
+
+func main() {
+	queue := worker.NewJobQueue(10)
+	pool := worker.NewWorkerPool(3)
+
+	worker.StartScheduler(queue, pool)
+
+	queue.Enqueue(&worker.Job{
+		ID:        "1",
+		Command:   "echo Job 1",
+		Status:    worker.StatusQueued,
+		CreatedAt: time.Now(),
+	})
+
+	queue.Enqueue(&worker.Job{
+		ID:        "2",
+		Command:   "sleep 2 && echo Job 2",
+		Status:    worker.StatusQueued,
+		CreatedAt: time.Now(),
+	})
+
+	queue.Enqueue(&worker.Job{
+		ID:        "3",
+		Command:   "echo Job 3",
+		Status:    worker.StatusQueued,
+		CreatedAt: time.Now(),
+	})
+
+	select {}
+}
