0d0cba5e51b6a5e1193b7a730e284d13dac939da
GIT_NAME <diagoarden@gmail.com>
2026-01-27 16:24:53 +0530

    updated the server code

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Welcome to our home page"))
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
